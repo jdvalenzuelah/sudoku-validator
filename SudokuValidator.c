5ef0b5cd2762fb3465e42018ff99999d8abdd3fe
David Valenzuela <davalenzuela@192.168.1.25>
2020-03-18 17:57:12 -0600

    INIT

---
+++ SudokuValidator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    
+    /* Verify sudoku file is being passed */
+    if(argc < 2)
+        printf("No sudoku file was passed"); return 1;
+
+}
